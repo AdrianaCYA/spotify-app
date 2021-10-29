aabb3ce854a55877c259c030e8b11137ccc90569
acastaneda <acastaneda@cyamexico.com>
2021-10-28 21:18:18 -0500

    a

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('', express.static(__dirname + '/dist/spotify-app'));
 
 //endpoints
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/dist/spotify-app/index.html')
+    res.sendFile(__dirname + '/dist/spotify-app/index.html');
 });
 
 app.listen(port, () => {
